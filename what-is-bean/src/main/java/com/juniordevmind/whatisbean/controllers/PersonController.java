6f81af8f00b2b6921be42bf4f0a5d02401e678ac
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-09-09 14:39:56 +0000

    try Component annotation

--- what-is-bean/src/main/java/com/juniordevmind/whatisbean/controllers/PersonController.java
+++ what-is-bean/src/main/java/com/juniordevmind/whatisbean/controllers/PersonController.java
@@ -5,17 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.juniordevmind.whatisbean.models.Person;
+import com.juniordevmind.whatisbean.utils.GreetingUtil;
 
 @RestController
 public class PersonController {
 
     @Autowired
     private Person _person;
+
+    @Autowired
+    private GreetingUtil _greetingUtil;
     
     @GetMapping("/try")
     public String tryAutowired() {
         _person.doIntro();
         _person.showOffCar();
         return "tried!";
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return _greetingUtil.greet();
     }
 }
---
+++ what-is-bean/src/main/java/com/juniordevmind/whatisbean/utils/GreetingUtil.java
@@ -0,0 +1,12 @@
+package com.juniordevmind.whatisbean.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingUtil {
+
+    public String greet() {
+        return "Hello";
+    }
+
+}
