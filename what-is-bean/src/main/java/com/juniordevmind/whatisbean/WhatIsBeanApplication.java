146f9c08ead47081adc771f02474df38b5dc8bf5
ryuichi24 <ryuichi.nishi24@gmail.com>
2022-09-09 13:52:52 +0000

    init spring project

---
+++ what-is-bean/src/main/java/com/juniordevmind/whatisbean/WhatIsBeanApplication.java
@@ -0,0 +1,13 @@
+package com.juniordevmind.whatisbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatIsBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatIsBeanApplication.class, args);
+	}
+
+}
---
+++ what-is-bean/src/test/java/com/juniordevmind/whatisbean/WhatIsBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniordevmind.whatisbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatIsBeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
